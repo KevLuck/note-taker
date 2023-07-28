b1652b5e5a4d587825ab3fa92e49e98b3c601301
kevluck <kevinl8700@gmail.com>
2023-07-27 22:05:16 -0400

    updated

--- helpers/file-io.js
+++ helpers/file-io.js
@@ -2,14 +2,13 @@ const { readFile, writeFile } = require("fs/promises");
 
 const writeToFile = async (destination, content) => {
   await writeFile(destination, JSON.stringify(content, null, 4));
-  console.info(`\nData written to ${destination}`);
 };
 
-const readAndAppend = async (content, file) => {
+const readAndAppend = async (file) => {
   const data = await readFile(file, "utf8");
   const parsedData = JSON.parse(data);
-  parsedData.push(content);
-  await writeFile(file, parsedData);
+  parsedData.push("New data");
+  await writeFile(file, JSON.stringify(parsedData, null, 4));
 };
 
 export { writeToFile, readAndAppend };
